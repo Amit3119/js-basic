b1ec005fa661df3a2d921588e07cdeebcd498f5d
Amit3119 <119948241+Amit3119@users.noreply.github.com>
2023-09-07 03:29:21 +0000

    varriables

---
+++ varriables/varriable_basic.js
@@ -0,0 +1,17 @@
+const accountId=144453;
+let accountEmail="abc@google.com";
+var accountPassword="12233";
+accountCity="jaipur";
+//
+// 
+//accountId=2 not allowed
+console.log(accountId);
+accountEmail="hc@hc.com"
+accountPassword="21111";
+accountCity="delhi";
+let accountState;
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+/*
+prefer not to use var 
+because of issue in functional scope and bloc scope
+*/
